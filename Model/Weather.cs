c58e8db801b3bde4a2b4b45db0c7be605472165a
ToxicSkill <gizmooos8@wp.pl>
2021-10-17 21:43:14 +0200

    separate classes for weather and web info

---
+++ Model/Weather.cs
@@ -0,0 +1,117 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+using System.IO;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CrytonCore.Model
+{
+    public class Weather
+    {
+        public WeatherInfo WholeForecast { get; set; }
+        public SingleWeather CurrnetWeather { get; set; }
+
+        public Weather()
+        {
+            Web web = new();
+            GetWeather(web.GetGlobalCoordinates());
+        }
+
+        public SingleWeather GetCurrentWeather()
+        {
+            return CurrnetWeather;
+        }
+        public WeatherInfo GetWholeForecast()
+        {
+            return WholeForecast;
+        }
+
+        private void GetWeather((double lat, double lon) geoLocation)
+        {
+            var weatherUrlStart = "http://www.7timer.info/bin/api.pl?";
+            StringBuilder stringBuilder = new(weatherUrlStart);
+            stringBuilder.Append("lon=" + geoLocation.lon + "&lat=" + geoLocation.lat);
+            stringBuilder.Append("&product=civil&output=json");
+            string respond = "";
+            WebRequest request = WebRequest.Create(stringBuilder.ToString());
+            using (WebResponse response = request.GetResponse())
+            using (StreamReader stream = new(response.GetResponseStream()))
+            {
+                respond = stream.ReadToEnd();
+            }
+            string info = new WebClient().DownloadString(stringBuilder.ToString());
+
+            SetWholeForecast(JsonConvert.DeserializeObject<WeatherInfo>(info));
+            SetCurrentWeather(FindCurrnetWeather());
+        }
+
+        private SingleWeather FindCurrnetWeather()
+        {
+            return WholeForecast.Dataseries.Single(x => x.Timepoint == "9");
+        }
+
+        private void SetWholeForecast(WeatherInfo weatherInfo)
+        {
+            WholeForecast = weatherInfo;
+        }
+        private void SetCurrentWeather(SingleWeather singleWeather)
+        {
+            CurrnetWeather = singleWeather;
+        }
+
+
+        public class WeatherInfo
+        {
+
+            [JsonProperty("product")]
+            public string Product { get; set; }
+
+            [JsonProperty("init")]
+            public string Init { get; set; }
+
+            [JsonProperty("dataseries")]
+            public IEnumerable<SingleWeather> Dataseries { get; set; }
+        }
+
+        public class SingleWeather
+        {
+
+            [JsonProperty("timepoint")]
+            public string Timepoint { get; set; }
+
+            [JsonProperty("cloudcover")]
+            public string Cloudcover { get; set; }
+
+            [JsonProperty("seeing")]
+            public string Seeing { get; set; }
+
+            [JsonProperty("transparency")]
+            public string Transparency { get; set; }
+
+            [JsonProperty("lifted_index")]
+            public string LiftedIndex { get; set; }
+
+            [JsonProperty("rh2m")]
+            public string Rh2m { get; set; }
+
+            [JsonProperty("wind10m")]
+            public Wind Wind10m { get; set; }
+
+            [JsonProperty("temp2m")]
+            public string Temp { get; set; }
+
+            [JsonProperty("prec_type")]
+            public string PrecType { get; set; }
+        }
+
+        public class Wind
+        {
+            [JsonProperty("direction")]
+            public string Direction { get; set; }
+
+            [JsonProperty("speed")]
+            public string Speed { get; set; }
+        }
+    }
+}
---
+++ Model/Web.cs
@@ -0,0 +1,106 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Net;
+
+namespace CrytonCore.Model
+{
+    public class Web
+    {
+        public JsonWeb WebInfo { get; set; }
+
+        public Web()
+        {
+            GetIPAddressPublic();
+        }
+
+        public JsonWeb GetAllWebInfo()
+        {
+            return WebInfo;
+        }
+
+        public (double latitude, double longnitude) GetGlobalCoordinates()
+        {
+            try
+            {
+                string info = new WebClient().DownloadString("http://ipinfo.io/" + WebInfo.Ip);
+                var gelocString = WebInfo.Loc.Split(',');
+                var resOne = double.Parse(gelocString[0], CultureInfo.InvariantCulture);
+                var resTwo = double.Parse(gelocString[1], CultureInfo.InvariantCulture);
+                return (latitude: resOne, longnitude: resTwo);
+            }
+            catch (Exception)
+            {
+                return (latitude: -1, longnitude: -1);
+            }
+        }
+
+        public string GetCurrentCity()
+        {
+            return WebInfo.City;
+        }
+        public string GetCurrentCountry()
+        {
+            return WebInfo.Country;
+        }
+        public string GetCurrentRegion()
+        {
+            return WebInfo.Region;
+        }
+        public string GetOrganization()
+        {
+            return WebInfo.Org;
+        }
+        public string GetPostalCode()
+        {
+            return WebInfo.Postal;
+        }
+        public string GetHostname()
+        {
+            return WebInfo.Hostname;
+        }
+
+        private void GetIPAddressPublic()
+        {
+            string address = "";
+            WebRequest request = WebRequest.Create("http://checkip.dyndns.org/");
+            using (WebResponse response = request.GetResponse())
+            using (StreamReader stream = new(response.GetResponseStream()))
+                address = stream.ReadToEnd();
+            int first = address.IndexOf("Address: ") + 9;
+            int last = address.LastIndexOf("</body>");
+            address = address.Substring(first, last - first);
+            string info = new WebClient().DownloadString("http://ipinfo.io/" + address);
+            WebInfo = JsonConvert.DeserializeObject<JsonWeb>(info);
+        }
+
+        public class JsonWeb
+        {
+
+            [JsonProperty("ip")]
+            public string Ip { get; set; }
+
+            [JsonProperty("hostname")]
+            public string Hostname { get; set; }
+
+            [JsonProperty("city")]
+            public string City { get; set; }
+
+            [JsonProperty("region")]
+            public string Region { get; set; }
+
+            [JsonProperty("country")]
+            public string Country { get; set; }
+
+            [JsonProperty("loc")]
+            public string Loc { get; set; }
+
+            [JsonProperty("org")]
+            public string Org { get; set; }
+
+            [JsonProperty("postal")]
+            public string Postal { get; set; }
+        }
+    }
+}
